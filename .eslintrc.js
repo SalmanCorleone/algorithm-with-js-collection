b5215f6da59bed36f06da7e3456ecf9393bfb924
salman <samilsalman12@gmail.com>
2020-08-06 21:38:23 +0600

    Set default structure

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  "parserOptions": {
+        "ecmaVersion": 2017
+    },
+
+    "env": {
+        "es6": true
+    },
+
+};
